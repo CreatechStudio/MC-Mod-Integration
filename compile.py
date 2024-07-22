efc65af8cb81eeb1635c18d893966081d23255a7
lightumcc <zzt404979156@163.com>
2024-07-22 17:46:21 +0800

    1.20.4b7

--- compile.py
+++ compile.py
@@ -35,7 +35,7 @@ override_ignore = [
 manifest = {
 	"name": "Createch Mod Integration Pack",
 	"author": "CreatechStudio Shanghai Inc.",
-	"version": "1.20.4b6",
+	"version": "1.20.4b7",
 	"description": "",
 	"fileApi": "https://mci.createchstudio.com",
 	"files": {},
